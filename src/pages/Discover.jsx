685b395e07cf4730a927c61bee3443d8212ee42b
1gabrielcarrizo <gabriel_glee_97@hotmail.com>
2023-01-21 20:00:33 -0300

    edigint playPause, songCard and discover components

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,8 +1,13 @@
+import {useDispatch, useSelector} from 'react-redux'
+
 import {Error, Loader, SongCard} from '../components'
 import { genres } from '../assets/constants';
 import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 
 const Discover = () => {
+
+  const dispatch = useDispatch()
+  const {activeSong, isPlaying} = useSelector((state) => state.player)
 
   const {data, isFetching, error} = useGetTopChartsQuery()
   const genreTitle = "Pop"
@@ -29,7 +34,10 @@ const Discover = () => {
             <SongCard
             key={song.key}
             song={song}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
             i={i}
+            data={data}
             />
           ))}
         </div>
