1db05a748d913eef52d7d3945ce13cf9c539e4a2
1gabrielcarrizo <gabriel_glee_97@hotmail.com>
2023-01-22 19:00:41 -0300

    editing page songDetails

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -65,7 +65,7 @@ const TopPlay = () => {
     <div ref={divRef} className='xl:ml-6 ml-0 xl:mb-0 mb-6 flex-1 xl:max-w-[500px] max-w-full flex flex-col'>
       <div className="w-full flex flex-col">
         <div className="flex flex-row justify-between items-center">
-          <h2 className="text-white font-bold text-2x1">Top Charts</h2>
+          <h2 className="text-white font-bold text-2xl">Top Charts</h2>
           <Link to='/top-charts'>
             <p className="text-gray-300 text-base cursor-pointer">
               See more
@@ -90,7 +90,7 @@ const TopPlay = () => {
 
       <div className="w-full flex flex-col mt-8">
       <div className="flex flex-row justify-between items-center">
-          <h2 className="text-white font-bold text-2x1">Top Artists</h2>
+          <h2 className="text-white font-bold text-2xl">Top Artists</h2>
           <Link to='/top-artists'>
             <p className="text-gray-300 text-base cursor-pointer">
               See more
